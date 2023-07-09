b363a0fba1e68ba42c68556c7d0c90ba4034969d
scybaloot <cdjsato@gmail.com>
2023-07-09 19:28:02 -0400

    Initial Commit

---
+++ stash/stashApp.swift
@@ -0,0 +1,20 @@
+//
+//  stashApp.swift
+//  stash
+//
+//  Created by Colby Sato on 7/9/23.
+//
+
+import SwiftUI
+
+@main
+struct stashApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
