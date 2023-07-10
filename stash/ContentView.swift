053b8b02525a9763d16fa49798818b214b2c3b9d
scybaloot <cdjsato@gmail.com>
2023-07-09 22:14:56 -0400

    Added Vision

--- stash/ContentView.swift
+++ stash/ContentView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import CoreData
+import Vision
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
--- stash/stashApp.swift
+++ stash/stashApp.swift
@@ -1,4 +1,5 @@
 //
+//
 //  stashApp.swift
 //  stash
 //
